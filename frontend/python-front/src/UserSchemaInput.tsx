238884cde5c668ddccccb9174a82e37cfd11992c
estwestminimu <t001masz@gmail.com>
2026-01-16 20:53:01 +0100

    vali

--- frontend/python-front/src/UserSchemaInput.tsx
+++ frontend/python-front/src/UserSchemaInput.tsx
@@ -33,7 +33,7 @@ function UserSchemaInput() {
       try {
       // @ts-ignore
         // const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
-        const API_URL = 'https://syllogism.onrender.com/'
+        const API_URL = 'https://syllogism.onrender.com'
 
         
         //Wysylamy dane do api wedlug naszych inputow
