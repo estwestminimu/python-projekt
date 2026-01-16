90c57cacc9c5700b0af1b98e4aca20fa54763be7
estwestminimu <t001masz@gmail.com>
2026-01-16 20:45:00 +0100

    url fix

--- frontend/python-front/src/UserSchemaInput.tsx
+++ frontend/python-front/src/UserSchemaInput.tsx
@@ -32,9 +32,9 @@ function UserSchemaInput() {
 
       try {
       // @ts-ignore
-        //  const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
+        const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
 
-        const API_URL = 'http://127.0.0.1:8000';
+        // const API_URL = 'http://127.0.0.1:8000';
         
         //Wysylamy dane do api wedlug naszych inputow
         const request = await axios.post(`${API_URL}/validation`,{
